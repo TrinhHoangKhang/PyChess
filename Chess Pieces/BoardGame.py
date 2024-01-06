8b078881850d923bd2846b72a3d6eff6f3785f7e
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2024-01-06 15:31:14 +0700

    Update
    Sửa lại hai class Chessman và BoardGame, các class còn lại chưa sửa

--- Chess Pieces/BoardGame.py
+++ Chess Pieces/BoardGame.py
@@ -4,18 +4,23 @@ class BoardGame:
     def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.chessmans = []  #Store chessmans in list
+        self.chessmans = []  # Store chessmans in list
+        self.movelog = []
     
+    # Add a chess piece to the board
     def add_chessman(self, chessman):
         self.chessmans.append(chessman)
     
+    # Check if there is a piece in x, y location
     def has_chessman(self, x, y):
         for chessman in self.chessmans:
-            if(chessman.current_x == x and chessman.current_y == y):
-                return chessman.enemy
-        return 0
+            if chessman.current_x_y == (x, y):
+                return True
+        return False
 
+    # Remove the piece from x, y location
     def remove_chessman(self, x, y):
         for chessman in self.chessmans:
-            if(chessman.current_x == x and chessman.current_y == y):
+            if chessman.current_x_y == (x, y):
                 self.chessmans.remove(chessman)
+                break
--- Chess Pieces/Chessman.py
+++ Chess Pieces/Chessman.py
@@ -1,18 +1,22 @@
 class Chessman:
-    def __init__(self, x, y, enemy, boardGame):
+    def __init__(self, x, y, color, boardGame):
         self.current_x_y = (x, y)
-        self.enemy = enemy  #Check if chessman is enemy or not, if it is enemy, value is 1, if not value is -1
-        self.enable_coordinates = {}  # Store step to move (-1, 0), (1, 0), (0, 1), ...
+        self.enemy = color  # b = black, w = white
+        self.possible_move = {}  # Store step to move (-1, 0), (1, 0), (0, 1), ...
         self.boardGame = boardGame
     
-    def set_enable_coordinates(self):
+    # Get the possible move, dont care about checkmate
+    def get_possible_move(self):
         pass
     
-    def move(self, x, y):
+    # Get the valid move, check checkmate
+    def get_valid_move(self):
         pass
-    
-    def beat(self, x, y):
-        self.move(x, y)
-        print(f"Beat at {x, y}")
 
+    # Move the piece to the given coordinate
+    def move(self, end_x, end_y):
+        pass
     
+    def capture(self, x, y):
+        self.move(x, y)
+        print(f"Capture at {x, y}")
