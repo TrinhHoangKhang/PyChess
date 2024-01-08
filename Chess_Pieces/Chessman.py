d76b85bb470189fe175dfcc3b8a39750edf5bd67
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2024-01-08 10:07:43 +0700

    Sửa chữ beat thành capture

--- Chess_Pieces/Chessman.py
+++ Chess_Pieces/Chessman.py
@@ -12,8 +12,8 @@ class Chessman:
     def move(self, x, y):
         pass
     
-    def beat(self, x, y):
-        self.move(x, y)
+    def capture(self, x, y):
+        self.capture(x, y)
         print(f"Beat at {x, y}")
 
     
